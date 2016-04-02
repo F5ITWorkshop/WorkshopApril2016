03a6c9f46883840d720661146b8cde76227a8d00
F5ITWorkshop <rma@f5it.ro>
2016-04-02 19:16:25 +0300

    Add search field in books list page; Add button that toggles between showing all the books or only the favorited books;

--- BookLibrary/BookLibrary.WebApp/scripts/books/booksController.js
+++ BookLibrary/BookLibrary.WebApp/scripts/books/booksController.js
@@ -1,11 +1,46 @@
-﻿app.controller('BooksController', function ($scope, BooksFactory) {
+﻿app.controller('BooksController', function ($scope, $filter, BooksFactory) {
+
+    $scope.search = { text: '' };
+
+    $scope.showOnlyFavorites = false;
 
     $scope.books = BooksFactory.getAllBooks();
+
+    var allBooks = [];
+
+    // first phase this works
+    //allBooks = angular.copy($scope.books);
+
+    // second phase this will work
+    $scope.books.$promise.then(function () {
+        allBooks = angular.copy($scope.books);
+    });
 
     $scope.toggleFavorite = function (book) {
         book.Favorite = !book.Favorite;
 
         BooksFactory.favoriteBook(book);
+    };
+
+    $scope.filterByTitle = function (book) {
+        if ($scope.search.text.length > 0) {
+            if ((book.Title).toLowerCase().indexOf(($scope.search.text).toLowerCase()) < 0) {
+                return false
+            }
+        }
+
+        return true;
+    };
+
+    $scope.toggleShowOnlyFavorites = function () {
+        $scope.showOnlyFavorites = !$scope.showOnlyFavorites;
+    
+        if ($scope.showOnlyFavorites) {
+            $scope.books = $filter('filter')(allBooks, { 'Favorite': $scope.showOnlyFavorites });
+        } else {
+            $scope.books = allBooks;
+        }
+        
     };
 
 });
